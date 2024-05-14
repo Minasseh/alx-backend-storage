034620ff32d1c4eb13e02de3ee7b6a77976b0d41
root <mnsalaro@yahoo.com>
2024-05-14 06:02:12 -0700

    Changes

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -7,4 +7,5 @@ def insert_school(mongo_collection, **kwargs):
     """ Inserts a new document in a collection based on kwargs and
     Returns the new _id """
     for key, value in kwargs.items():
-        mongo_collection.insert_many([{key: value}])
+        result = mongo_collection.insert_many([{key: "value"}])
+    return result.inserted_ids
