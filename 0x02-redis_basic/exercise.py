bafe5bf5e3dd2b4cf744c104addbe45d2ea4d9b9
root <mnsalaro@yahoo.com>
2024-05-15 09:09:58 -0700

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,7 +17,7 @@ class Cache:
         """  A store method that takes a data argument and returns a string.
         The method should generate a random key (e.g. using uuid), store the
         input data in Redis using the random key and return the key. """
-        random_key = uuid.uuid4()
+        random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
 
         return random_key
